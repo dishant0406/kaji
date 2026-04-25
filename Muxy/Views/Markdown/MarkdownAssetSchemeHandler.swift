c0bd7f5ac4b77e8a319fc1d021312659d01836fe
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2026-04-25 14:14:23 +0200

    Fix mermaid bundle crash

--- Muxy/Views/Markdown/MarkdownAssetSchemeHandler.swift
+++ Muxy/Views/Markdown/MarkdownAssetSchemeHandler.swift
@@ -26,7 +26,7 @@ final class MarkdownAssetSchemeHandler: NSObject, WKURLSchemeHandler {
 
         let filename = url.lastPathComponent
         guard Self.allowedFiles.contains(filename),
-              let resourceURL = Bundle.module.url(forResource: filename, withExtension: nil),
+              let resourceURL = Bundle.appResources.url(forResource: filename, withExtension: nil),
               let data = try? Data(contentsOf: resourceURL)
         else {
             urlSchemeTask.didFailWithError(URLError(.fileDoesNotExist))
