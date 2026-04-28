47b4fa4eec8e1855e91c949ebdbda24c3ab3045c
Dishant <dishu5570@gmail.com>
2026-04-28 16:44:27 +0530

    Add resource monitor snapshot model

---
+++ Droid/Models/ResourceMonitorSnapshot.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+struct ResourceMonitorTerminalDescriptor: Identifiable, Equatable {
+    let paneID: UUID
+    let tabID: UUID
+    let areaID: UUID
+    let projectID: UUID
+    let projectName: String
+    let title: String
+
+    var id: UUID { paneID }
+}
+
+struct ResourceMonitorTerminalReading: Identifiable, Equatable {
+    let descriptor: ResourceMonitorTerminalDescriptor
+    let pid: Int32?
+    let processName: String?
+    let ttyName: String?
+    let cpuPercent: Double?
+    let memoryBytes: UInt64?
+    let threadCount: Int?
+
+    var id: UUID { descriptor.id }
+}
+
+struct ResourceMonitorTerminalSnapshot: Identifiable, Equatable {
+    let paneID: UUID
+    let tabID: UUID
+    let areaID: UUID
+    let projectID: UUID
+    let title: String
+    let pid: Int32?
+    let processName: String?
+    let ttyName: String?
+    let cpuPercent: Double?
+    let memoryBytes: UInt64?
+    let threadCount: Int?
+
+    var id: UUID { paneID }
+}
+
+struct ResourceMonitorProjectSnapshot: Identifiable, Equatable {
+    let id: UUID
+    let name: String
+    let cpuPercent: Double
+    let memoryBytes: UInt64
+    let terminals: [ResourceMonitorTerminalSnapshot]
+}
